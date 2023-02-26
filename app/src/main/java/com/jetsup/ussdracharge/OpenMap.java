9dbfd5a31f4c1f895adfba3676635e795e91f138
George Ngigi <ngigegeorge023@gmail.com>
2023-02-26 21:52:39 +0300

    Added marker to map

--- app/src/main/java/com/jetsup/ussdracharge/OpenMap.java
+++ app/src/main/java/com/jetsup/ussdracharge/OpenMap.java
@@ -2,12 +2,14 @@ package com.jetsup.ussdracharge;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.PreferenceManager;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -16,20 +18,28 @@ import com.google.android.gms.location.LocationServices;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.OverlayItem;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class OpenMap extends AppCompatActivity {
     final String TAG = "MyTag";
     private final int LOCATION_REQUEST_CODE = 100;
     ItemizedOverlay<OverlayItem> overlayItems;
     MapView mapView;
+    MapController mapController;
     //    LocationRequest locationRequest;
     FusedLocationProviderClient fusedLocationProviderClient;
     private boolean hasLocationPermission;
+    private List<Overlay> overlayItemList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +52,35 @@ public class OpenMap extends AppCompatActivity {
         mapView = findViewById(R.id.mapView);
         // set mapView tile source
         mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapController = (MapController) mapView.getController();
         // get the last known user location
-//        locationRequest = LocationRequest.create();
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
-//        hasLocationPermission = getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-//                && getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
         if (!permissionDenied()) {
             hasLocationPermission = true;
             updateLocation(); // if the user does not give permission, show default location
+        }
+    }
+
+    public void addMarker(GeoPoint loc, boolean myLocation) {
+        Marker marker = new Marker(mapView);
+        marker.setPosition(loc);
+        if (myLocation) {
+            marker.setTitle("Your last known location");
+        } else {
+            marker.setTitle("Location of interest");
+        }
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Drawable redLocationMarker = ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_location_on_24, null);
+        Objects.requireNonNull(redLocationMarker).setTint(getColor(R.color.airtel_red));
+        marker.setIcon(redLocationMarker);
+        mapView.getOverlays().clear();
+        overlayItemList.add(marker);
+        for (Overlay overlay : overlayItemList) {
+            mapView.getOverlays().add(overlay);
+        }
+        mapView.postInvalidate();
+        if (myLocation) {
+            mapController.animateTo(loc, 18.0, 1000L);
         }
     }
 
@@ -58,13 +89,18 @@ public class OpenMap extends AppCompatActivity {
             if (hasLocationPermission) {
                 fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
                             // set the mapView initial zoom level and center point
-                            mapView.getController().setZoom(15.0);
-                            mapView.getController().setCenter(new GeoPoint(location.getLatitude(), location.getLongitude()));
+                            addMarker(new GeoPoint(location.getLatitude(), location.getLongitude()), true);
+                            addMarker(new GeoPoint(location.getLatitude() + 0.3, location.getLongitude() + 0.2), false);
+//                            mapView.getController().animateTo(new GeoPoint(location.getLatitude(), location.getLongitude()));
+//                            mapView.getController().setZoom(20.0);
+                            Log.d(TAG, "updateLocation: has permission");
+//                            mapView.getController().setCenter(new GeoPoint(location.getLatitude(), location.getLongitude()));
                         }
                 );
             } else {
                 mapView.getController().setZoom(10.0);
                 mapView.getController().setCenter(new GeoPoint(38.9, -9.6));
+                Log.d(TAG, "updateLocation: has no permission");
             }
         } catch (SecurityException ignore) {
         }
